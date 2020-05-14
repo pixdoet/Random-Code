4e2e270dfe5ee97f6df7b0460850a3cdbe4e6000
pixdoet <63037136+pixdoet@users.noreply.github.com>
2020-05-14 15:24:17 +0800

    random color generator.
    Put 3 LEDs or just use a multicoloured one

--- randomLED.ino
+++ randomLED.ino
@@ -1,4 +1,5 @@
-//led circuits. Prepare for explosions
+//Led circuits. Prepare for explosions
+//3V is 512 in AnalogLand. Do not exceed or the led will explode
 
 //various ints for led analog pin no.
 int red = A0;
@@ -7,19 +8,20 @@ int green = A2;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  //begin serial mon
   Serial.begin(9600);
+  //test serial mon
   Serial.println("hello world");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   /*
    * generates random signals for the
    * leds that are 3 volt only
    */
   
+ 
   int redvolt = random(0, 512);
   int bluevolt = random(0, 512);
   int greenvolt = random(0, 512);
