bb8b88dfea392174ab8b9c1472736483383b85b0
pixdoet <63037136+pixdoet@users.noreply.github.com>
2020-09-25 22:47:57 +0800

    Added read and exit feature on select file action

--- worder.py
+++ worder.py
@@ -1,32 +1,48 @@
 # ikr this is weird
 # a word processor made in python entirely i
 
-def mainApp():
-    quitToHome = False
-    while quitToHome == False:
-        fileName = raw_input("Please enter a file name, a valid one to open it: ")
+def mainAppCommand():
+    quitCommand = False
+    while quitCommand == False:
+        print ("")
+        print ("Please enter a file name, a valid one to open it: ")
+        fileName = raw_input(">")
         f = open(fileName, "a")
-        inputText = raw_input("Please enter the contents of your text: ")
-        if inputText == "commandExit":
-            quitToHome = True
-        f.write(inputText)
+        print ("Please type R to read this file, or type W to write this file. Type X to exit. Upper case or lower case does not matter.")
+        fileCommand = raw_input(">")
+        if fileCommand == "R" or fileCommand == "r":
+            openr = open(fileName, "r")
+            print (openr.read())
+            mainAppCommand()
+        elif fileCommand == "W" or fileCommand == "w":
+            openf = open(fileName, "w")
+            writeContents = raw_input(">")
+            openf.write(writeContents)
+            mainAppCommand()
+        elif fileCommand == "x" or fileCommand == "X":
+            quitCommand = True
+        else:
+            print ("Invalid command. Please type W, w, R or r")
+            mainAppCommand()
 
-    if quitToHome == True:
-        f.close()
+    if quitCommand == True:
         homeScreen()
-
+            
+        
 
 
 def creditsApp():
-    print ("Worder version alpha 1.0")
+    print ("")
+    print ("Worder Version Alpha 1.1")
     print ("Created by Jun Ian. Tested with various computers and operating systems")
-    print ("The source code is available on Github: https://github.com/pixdoet/random_code/worder.py")
-    print ("Thank you for using Worder. Updates will be available as soon as I'm free")
+    print ("The source code is available on Github: https://github.com/pixdoet/Random-Code/blob/master/worder.py")
+    print ("Thank you for using Worder! Updates will be available as soon as I'm free")
     print ("Suggest updates by emailing to interexchangesss@gmail.com or messaging me on Facebook: Jun Ian")
     homeScreen()
     
 def homeScreen():
-    print ("Simple Word Processor Version 1.0")
+    print ("")
+    print ("Simple Word Processor Version 1.1")
     print ("Copyright (C) 2020 Jun Ian. Not copyrighted actually")
     print ("Written in Python 2.7")
     print ("Report issues by private messaging me on Facebook Messenger: Jun Ian")
@@ -35,17 +51,17 @@ def homeScreen():
         print ("Type start to start word processing!")
         print ("Type credits to view credits!")
         print ("Type exit to exit help and return to the home screen")
-        helpCmd = raw_input("Command: ")
+        helpCmd = raw_input(">")
         if helpCmd == "exit":
             homeScreen()
         elif helpCmd == "credits":
             creditsApp()
         elif helpCmd == "start":
-            mainApp()
+            mainAppCommand()
         else:
             homeScreen()
     elif uiCmd == "start":
-        mainApp()
+        mainAppCommand()
     elif uiCmd == "credits":
         creditsApp()
     elif uiCmd == "exit":
@@ -55,7 +71,5 @@ def homeScreen():
 
 homeScreen()
 
-
-
-        
-        
+# the source code does not compile. I need help
+# created by jun ian on 25 sept 2020. uploaded on 2247 GMT +8
